ed5c1173b376184b8356446bd538750796fd1d9c
elilaiono <elilaiono@gmail.com>
2022-10-07 09:56:56 -0600

    i hope this works

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const {v4 : uuidv4} = require("uuid");
+const port = 3000;
+const app = express(0);
+const {createClient} = require('redis');
+const md5 = require('md5');
+
+const redisClient = createClient(
+    {
+    Url:"redis://default@localhost:6379",
+    }
+);
+
+app.use(bodyParser.json());
+
+app.listen(port, async ()=>{
+    await redisClient.connect();
+    console.log('listening on port'+port);
+    
+});
+
+app.get('/', (req,res)=>{
+    res.send('Hello World')
+});
+
+app.post('/user', (req, res)=>{
+    const newUserRequestObject = req.body;
+    console.log('New User:',JSON.stringify(newUserRequestObject));
+    redisClient.hSet('users',req.body.email,JSON.stringify(newUserRequestObject));
+    res.send('New user'+newUserRequestObject.email+'added');
+});
+
+// redis-cli
+// ./signup.cmd
+
+app.post("/login",async (req,res)=>{
+    const loginEmail = req.body.userName;
+    console.log(JSON.stringify(req.body));
+    console.log("loginEmail", loginEmail);
+    const loginPassword = req.body.password;
+    console.log("loginPassword", loginPassword);
+    // res.send("Who are you");
+
+    const userString=await redisClient.hGet('users',loginEmail);
+    const userObject=JSON.parse(userString)
+    if(userString=='' || userString==null) {
+        res.status(404);
+        res.send('User not found');
+    }
+    else if (loginEmail == userObject.userName && loginPassword == userObject.password){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized
+        res.send("Invalid user or password");
+    }
+})
